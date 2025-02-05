bbcbd76ecbf11b17220abf47b52198815bef3744
unknown <mariemesembenediop@gmail.com>
2025-02-05 01:37:20 +0000

    fin

--- labo1/labo1.ino
+++ labo1/labo1.ino
@@ -1,48 +1,50 @@
-// Remplacer "2142752" par votre numéro d'étudiant
-const int ledPin = 13;
-const int delayDuration = 2000; // Délai de 2 secondes
-const int studentNumber[] = {2, 1, 4, 2, 7, 5, 2}; // Numéro d'étudiant, à adapter
+const int LED_PIN = 13;
+const int NUMBER_OF_FLASHES = 4;
+const int DELAY_FLASHES = 250;
+const int DELAY_DURATION = 2000;
+const int INTENSITY_VARIATION = 2048 / 256;
+
+void EtatAllumer() {
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("Allume - 2415655");
+  delay(DELAY_DURATION);
+}
+
+void EtatClignotement() {
+  for (int i = 0; i < NUMBER_OF_FLASHES; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(DELAY_FLASHES);
+    digitalWrite(LED_PIN, LOW);
+    delay(DELAY_FLASHES);
+    
+  }
+   Serial.println("Clignotement - 2415655");
+}
+
+void EtatVariation() {
+  
+  for (int i=255; i >= 0; i--) {
+    analogWrite(LED_PIN, i);
+    delay(INTENSITY_VARIATION);
+     
+  }
+  Serial.println("variation - 2415655");
+}
+
+void EtatEteint() {
+  digitalWrite(LED_PIN, LOW);
+  Serial.println("Eteint - 2415655");
+  delay(DELAY_DURATION);
+}
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600); // Initialisation de la communication série
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // État 1 : Allumé
-  digitalWrite(ledPin, HIGH);
-  Serial.println("Allume - 2142752");
-  delay(delayDuration);
-
-  // État 2 : Clignotement
-  int avantDernierChiffre = studentNumber[5];
-  int clignotements = ceil(avantDernierChiffre / 2.0) + 1;
-  if (avantDernierChiffre == 0) clignotements = 10;
-  Serial.println("Clignotement - 2142752");
-  for (int i = 0; i < clignotements; i++) {
-    digitalWrite(ledPin, HIGH);
-    delay(250); // 250 ms allumé
-    digitalWrite(ledPin, LOW);
-    delay(250); // 250 ms éteint
-  }
-
-  // État 3 : Variation d'intensité
-  int dernierChiffre = studentNumber[6];
-  Serial.println("Variation - 2142752");
-  if (dernierChiffre % 2 == 0) {
-    for (int i = 0; i <= 255; i++) { // Augmente l'intensité graduellement
-      analogWrite(ledPin, i);
-      delay(8); // 2048 ms divisé par 256 niveaux
-    }
-  } else {
-    for (int i = 255; i >= 0; i--) { // Diminue l'intensité graduellement
-      analogWrite(ledPin, i);
-      delay(8); // 2048 ms divisé par 256 niveaux
-    }
-  }
-
-  // État 4 : Éteint
-  digitalWrite(ledPin, LOW);
-  Serial.println("Eteint - 2142752");
-  delay(delayDuration);
+  EtatAllumer();
+   EtatClignotement();
+  EtatVariation();
+  EtatEteint();
 }
